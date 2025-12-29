8ae6116d1c95f5345ea32466297b7a3e517c77f6
Ayrat <kiamovajrat1@gmail.com>
2025-12-29 21:53:40 +0300

    create dockerfile and dockercompose

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.responses import FileResponse
 
 from database.database import create_db_and_tables
 from contextlib import asynccontextmanager
 from routers import expense, amount, filter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     create_db_and_tables()
+    print("Server start! Open: http://localhost:8000/")
     yield
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
@@ -18,13 +21,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+@app.get("/")
+def red_root():
+    return FileResponse("frontend/index.html")
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 app.include_router(expense.router)
 app.include_router(amount.router)
 app.include_router(filter.router)
 
-@app.get("/")
-def first_page():
-    return {"message": "Hello World"}
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
